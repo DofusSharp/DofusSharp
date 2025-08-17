2208b059dc8c26db25bbc84725739e8492a33d2b
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-08-17 17:49:11 +0200

    reduce test duration by reducing amount of data queries

--- Tests.EndToEnd.DofusDb.ApiClients/ItemsClientTest.cs
+++ Tests.EndToEnd.DofusDb.ApiClients/ItemsClientTest.cs
@@ -30,9 +30,9 @@ public class ItemsClientTest
 
         // we don't want to assert results here because they might change with each update, we just want to ensure that all the items are parsed correctly
         // which means that no exception is thrown during the search
-        SearchQuery query = new();
+        SearchQuery query = new() { Predicates = [new SearchPredicate.GreaterThanOrEqual("level", "190")] };
         Item[] results = await client.MultiQuerySearchAsync(query).ToArrayAsync();
-        int count = await client.CountAsync();
+        int count = await client.CountAsync(query.Predicates);
 
         results.Length.Should().Be(count);
     }
