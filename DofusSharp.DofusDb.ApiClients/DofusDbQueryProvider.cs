d15d4df85f6514c54d8bbe1327d6e998d055e9aa
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-08-17 18:03:00 +0200

    add new apis to static query factory

--- DofusSharp.DofusDb.ApiClients/DofusDbQueryProvider.cs
+++ DofusSharp.DofusDb.ApiClients/DofusDbQueryProvider.cs
@@ -1,4 +1,5 @@
 ﻿using DofusSharp.DofusDb.ApiClients.Models.Items;
+using DofusSharp.DofusDb.ApiClients.Models.Maps;
 
 namespace DofusSharp.DofusDb.ApiClients;
 
@@ -8,4 +9,12 @@ public class DofusDbQueryProvider(DofusDbClientsFactory factory)
     public DofusDbQuery<ItemType> ItemTypes() => new(factory.ItemTypes());
     public DofusDbQuery<ItemSuperType> ItemSuperTypes() => new(factory.ItemSuperTypes());
     public DofusDbQuery<ItemSet> ItemSets() => new(factory.ItemSets());
+
+    public DofusDbQuery<World> Worlds() => new(factory.Worlds());
+    public DofusDbQuery<SuperArea> SuperAreas() => new(factory.SuperAreas());
+    public DofusDbQuery<Area> Areas() => new(factory.Areas());
+    public DofusDbQuery<SubArea> SubAreas() => new(factory.SubAreas());
+    public DofusDbQuery<Map> Maps() => new(factory.Maps());
+    public DofusDbQuery<MapPosition> MapPositions() => new(factory.MapPositions());
+    public DofusDbQuery<Dungeon> Dungeons() => new(factory.Dungeons());
 }
