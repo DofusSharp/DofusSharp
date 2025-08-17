514834ade8624f3ecf41f73d09bd4dd62494b5b6
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-08-17 18:46:47 +0200

    add tests

---
+++ Tests.UnitTests.DofusDb.ApiClients/DofusDbImageClientTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using DofusSharp.DofusDb.ApiClients;
+using DofusSharp.DofusDb.ApiClients.Models.Common;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Moq;
+using Moq.Contrib.HttpClient;
+using Tests.UnitTests.DofusDb.ApiClients.Extensions;
+
+namespace Tests.UnitTests.DofusDb.ApiClients;
+
+[TestSubject(typeof(DofusDbImageClient))]
+public class DofusDbImageClientTest
+{
+    [Theory]
+    [InlineData(ImageFormat.Jpeg, "jpg")]
+    [InlineData(ImageFormat.Png, "jpg")]
+    public async Task GetImage_Should_ReturnImage(ImageFormat imageFormat, string extension)
+    {
+        byte[] imageBytes = [4, 5, 6];
+        Mock<HttpMessageHandler> httpHandlerMock = new(MockBehavior.Strict);
+        httpHandlerMock.SetupRequest(HttpMethod.Get, $"http://base.com/123.{extension}").ReturnsResponse(HttpStatusCode.OK, imageBytes);
+        DofusDbImageClient client = new(new Uri("http://base.com"), imageFormat)
+        {
+            HttpClientFactory = httpHandlerMock.CreateClientFactory()
+        };
+
+        Stream imageStream = await client.GetImageAsync(123);
+
+        imageStream.ReadToByteArray().Should().BeEquivalentTo(imageBytes);
+    }
+
+    [Fact]
+    public async Task GetImage_Should_SetHttpParameters()
+    {
+        Mock<HttpMessageHandler> httpHandlerMock = new(MockBehavior.Strict);
+        httpHandlerMock.SetupRequest(HttpMethod.Get, "http://base.com/123.jpg").ReturnsResponse(HttpStatusCode.OK, "1.2.3.4");
+        DofusDbImageClient client = new(new Uri("http://base.com"), ImageFormat.Jpeg, new Uri("http://referrer.com"))
+        {
+            HttpClientFactory = httpHandlerMock.CreateClientFactory()
+        };
+
+        await client.GetImageAsync(123);
+
+        httpHandlerMock.VerifyRequest(HttpMethod.Get, "http://base.com/123.jpg", req => req.Headers.Referrer == new Uri("http://referrer.com"));
+    }
+}
