50d4075cfe900adbc68cd79723ab91cb1e71bda6
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-08-24 00:18:18 +0200

    add search

--- BestCrush/Components/KnownRoutes.cs
+++ BestCrush/Components/KnownRoutes.cs
@@ -9,10 +9,4 @@ static class KnownRoutes
     public static string Servers() => "/servers";
     public static string Server(string server) => $"/servers/{server}";
     public static string Server(DofocusServer server) => Server(server.Name);
-
-    public static string Crush(string server) => $"/servers/{server}/crush";
-    public static string Crush(DofocusServer server) => Crush(server.Name);
-
-    public static string SalesHouse(string server) => $"/servers/{server}/sales";
-    public static string SalesHouse(DofocusServer server) => SalesHouse(server.Name);
 }
