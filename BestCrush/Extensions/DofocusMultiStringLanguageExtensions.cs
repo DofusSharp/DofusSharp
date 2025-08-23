7428d2cf5fcaa817fcfca17001e9ebe00d349ab6
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-08-24 01:54:23 +0200

    add language selection

---
+++ BestCrush/Extensions/DofocusMultiStringLanguageExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using DofusSharp.Dofocus.ApiClients.Models.Common;
+
+namespace BestCrush.Extensions;
+
+static class DofocusMultiStringLanguageExtensions
+{
+    public static string Localize(this DofocusMultiLangString str)
+    {
+        CultureInfo culture = CultureInfo.CurrentUICulture;
+        return culture.Name switch
+        {
+            "en-US" or "en" => str.En,
+            "fr-FR" or "fr" => str.Fr,
+            "es-ES" or "es" => str.Es,
+            _ => str.En
+        };
+    }
+}
