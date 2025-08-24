341fc9fc969a84dd248e4582cb0298594072f1c2
Ismail Bennani <ismail.lahkim.bennani@gmail.com>
2025-08-24 12:54:26 +0200

    feat: allow some fields of Dofocus models to be null (#27)

--- DofusSharp.Dofocus.ApiClients/Models/Common/DofocusMultiLangString.cs
+++ DofusSharp.Dofocus.ApiClients/Models/Common/DofocusMultiLangString.cs
@@ -8,15 +8,15 @@ public class DofocusMultiLangString
     /// <summary>
     ///     The French translation.
     /// </summary>
-    public required string Fr { get; init; }
+    public string? Fr { get; init; }
 
     /// <summary>
     ///     The English translation.
     /// </summary>
-    public required string En { get; init; }
+    public string? En { get; init; }
 
     /// <summary>
     ///     The Spanish translation.
     /// </summary>
-    public required string Es { get; init; }
+    public string? Es { get; init; }
 }
