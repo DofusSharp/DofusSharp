c98f3da212f8f90ad93421b7f9eef9f7ec99de2f
Ismail Bennani <ismail.lahkim.bennani@gmail.com>
2025-08-30 12:43:41 +0200

    feat: change collections of DofusDbRecipe to use IReadOnlyList (#30)

--- DofusSharp.DofusDb.ApiClients/Models/Jobs/DofusDbRecipe.cs
+++ DofusSharp.DofusDb.ApiClients/Models/Jobs/DofusDbRecipe.cs
@@ -8,15 +8,15 @@ public class DofusDbRecipe : DofusDbResource
     public long? ResultId { get; init; }
     public long? ResultTypeId { get; init; }
     public int? ResultLevel { get; init; }
-    public IReadOnlyCollection<long>? IngredientIds { get; init; }
-    public IReadOnlyCollection<int>? Quantities { get; init; }
+    public IReadOnlyList<long>? IngredientIds { get; init; }
+    public IReadOnlyList<int>? Quantities { get; init; }
     public long? JobId { get; init; }
     public long? SkillId { get; init; }
     public string? ChangeVersion { get; init; }
     public long? TooltipExpirationDate { get; init; }
     public DofusDbMultiLangString? ResultName { get; init; }
     public DofusDbItem? Result { get; init; }
-    public IReadOnlyCollection<DofusDbItem>? Ingredients { get; init; }
+    public IReadOnlyList<DofusDbItem>? Ingredients { get; init; }
     public DofusDbJob? Job { get; init; }
     public DofusDbItemType? ResultType { get; init; }
 }
