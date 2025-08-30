76177b3321bcf9b7c3f140b763cfdbf9dadf77d0
Ismail Bennani <ismail.lahkim.bennani@gmail.com>
2025-08-30 13:06:00 +0200

    feat: add dofus db servers client (#31)

---
+++ DofusSharp.DofusDb.ApiClients/Models/Servers/DofusDbLanguage.cs
@@ -0,0 +1,7 @@
+﻿namespace DofusSharp.DofusDb.ApiClients.Models.Servers;
+
+public enum DofusDbLanguage
+{
+    En,
+    Fr
+}
---
+++ DofusSharp.DofusDb.ApiClients/Serialization/DofusDbDateOnlyJsonConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DofusSharp.DofusDb.ApiClients.Serialization;
+
+/// <summary>
+/// </summary>
+class DofusDbDateOnlyJsonConverter : JsonConverter<DateOnly>
+{
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        switch (reader.TokenType)
+        {
+            case JsonTokenType.Number:
+                double value = JsonSerializer.Deserialize<double>(ref reader, options);
+                DateTime dateTime = DateTime.UnixEpoch.AddMilliseconds(value);
+                return DateOnly.FromDateTime(dateTime);
+            default:
+                return JsonSerializer.Deserialize<DateOnly>(ref reader, options);
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options) => JsonSerializer.Serialize(writer, value, options);
+}
