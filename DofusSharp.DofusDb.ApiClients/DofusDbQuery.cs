ba3b4c7a5a4b45ade4dbac4bb790270358ee5e22
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-09-07 22:15:05 +0200

    add create method to create dummy version of query in tests

--- DofusSharp.DofusDb.ApiClients/DofusDbQuery.cs
+++ DofusSharp.DofusDb.ApiClients/DofusDbQuery.cs
@@ -7,12 +7,18 @@ public static class DofusDbQuery
     /// <summary>
     ///     Create a query provider for the DofusDb API that connects to the production API.
     /// </summary>
+    /// <param name="clientsFactory">The clients factory used by the query.</param>
+    public static IDofusDbQueryProvider Create(IDofusDbClientsFactory clientsFactory) => new DofusDbQueryProvider(clientsFactory);
+
+    /// <summary>
+    ///     Create a query provider for the DofusDb API that connects to the production API.
+    /// </summary>
     /// <param name="referrer">The referer header to include in requests to the API.</param>
-    public static IDofusDbQueryProvider Production(Uri? referrer = null) => new DofusDbQueryProvider(DofusDbClient.Production(referrer));
+    public static IDofusDbQueryProvider Production(Uri? referrer = null) => Create(DofusDbClient.Production(referrer));
 
     /// <summary>
     ///     Create a query provider for the DofusDb API that connects to the beta API.
     /// </summary>
     /// <param name="referrer">The referer header to include in requests to the API.</param>
-    public static IDofusDbQueryProvider Beta(Uri? referrer = null) => new DofusDbQueryProvider(DofusDbClient.Beta(referrer));
+    public static IDofusDbQueryProvider Beta(Uri? referrer = null) => Create(DofusDbClient.Beta(referrer));
 }
