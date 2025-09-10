be844ab8bcf4af88a45d0dda5e9b70bd2f369d0a
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-09-10 22:52:20 +0200

    wip

---
+++ DofusSharp.DofusDb.Cli/Commands/ItemsCommand.cs
@@ -0,0 +1,43 @@
+﻿using System.CommandLine;
+using DofusSharp.DofusDb.ApiClients.Models;
+using DofusSharp.DofusDb.ApiClients.Models.Items;
+
+namespace DofusSharp.DofusDb.Cli.Commands;
+
+public abstract class TableClientCommand<TResource> where TResource: DofusDbResource
+{
+    public abstract string Command { get; }
+    public abstract string Name { get; }
+
+    public Command CreateCommand() =>
+        new(Name, $"Query {Name.ToLowerInvariant()} resources from DofusDB.")
+        {
+            CreateSearchCommand(),
+            CreateGetCommand(),
+            CreateCountCommand()
+        };
+
+    Command CreateSearchCommand() => new("search", $"Search for {Name.ToLowerInvariant()}.");
+
+    Command CreateGetCommand() =>
+        new("get", $"Get {Name.ToLowerInvariant()} by id.")
+        {
+            Arguments =
+            {
+                new Argument<long>("id")
+                {
+                    Description = "The unique identifier of the resource.",
+                    Arity = ArgumentArity.ExactlyOne,
+                    HelpName = "ID"
+                }
+            }
+        };
+
+    Command CreateCountCommand() => new("count", $"Count {Name.ToLowerInvariant()}.");
+}
+
+public class ItemsCommand : TableClientCommand<DofusDbItem>
+{
+    public override string Command => "items";
+    public override string Name => "Items";
+}
--- DofusSharp.DofusDb.Cli/Program.cs
+++ DofusSharp.DofusDb.Cli/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.CommandLine;
+using System.CommandLine.Parsing;
+using DofusSharp.DofusDb.Cli.Commands;
+
+RootCommand rootCommand = new("A command line interface for DofusDB.")
+{
+    new ItemsCommand().CreateCommand()
+};
+
+ParseResult parseResult = rootCommand.Parse(args);
+
+if (parseResult.Errors.Count != 0)
+{
+    foreach (ParseError parseError in parseResult.Errors)
+    {
+        Console.Error.WriteLine(parseError.Message);
+    }
+    return 1;
+}
+
+return await parseResult.InvokeAsync();
