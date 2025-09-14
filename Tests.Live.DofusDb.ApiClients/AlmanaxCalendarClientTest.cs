e41fb9abadbb1905759b1130bc2a5f5edf904800
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-09-14 15:24:32 +0200

    add test

---
+++ Tests.Live.DofusDb.ApiClients/AlmanaxCalendarClientTest.cs
@@ -0,0 +1,15 @@
+﻿using DofusSharp.DofusDb.ApiClients;
+using DofusSharp.DofusDb.ApiClients.Models.Almanax;
+
+namespace Tests.Live.DofusDb.ApiClients;
+
+public class AlmanaxCalendarClientTest
+{
+    [Fact]
+    public async Task AlmanaxCalendarClient_Should_GetAlmanaxCalendar()
+    {
+        IDofusDbAlmanaxCalendarClient client = DofusDbClient.Beta(Constants.Referrer).Almanax();
+        DofusDbAlmanaxCalendar value = await client.GetAlmanaxCalendarAsync(new DateOnly(1995, 06, 03));
+        await Verify(value);
+    }
+}
