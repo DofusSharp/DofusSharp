609390a62858bd57bd9f3e2c874d5945201394af
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-09-14 12:13:38 +0200

    reduce requests in test

--- Tests.Live.DofusDb.ApiClients/AchievementObjectivesClientTest.cs
+++ Tests.Live.DofusDb.ApiClients/AchievementObjectivesClientTest.cs
@@ -22,10 +22,9 @@ public class AchievementObjectivesClientTest
 
         // we don't want to assert results here because they might change with each update, we just want to ensure that all the items are parsed correctly
         // which means that no exception is thrown during the search
-        DofusDbSearchQuery query = new();
+        DofusDbSearchQuery query = new() { Limit = 1000 };
         DofusDbAchievementObjective[] results = await client.MultiQuerySearchAsync(query).ToArrayAsync();
-        int count = await client.CountAsync(query.Predicates);
 
-        results.Length.Should().Be(count);
+        results.Length.Should().Be(1000);
     }
 }
